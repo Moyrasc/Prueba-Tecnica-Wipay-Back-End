2839171d53cefd492b4e329263cc2831af42280f
Moyrasc <noemisolac@gmail.com>
2023-07-26 11:31:28 +0200

    add passport authenticate in route

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
-// import passport from 'passport'
+import passport from 'passport'
 import routerLogin from './routes/login.js'
 import routerUsers from './routes/users.js'
+import './auth/auth.js'
 const app = express()
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.send('Empezando con el Back')
 })
 // Routes
-app.use('login', routerLogin)
-app.use('/users', routerUsers)
+app.use('/login', routerLogin)
+app.use('/users', passport.authenticate('jwt', { session: false }), routerUsers)
 
 app.use((req, res) => {
   res.status(404).send({
